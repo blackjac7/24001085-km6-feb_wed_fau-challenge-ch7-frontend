9568f9fa7dd8a0078b55e0c0d357bbe1aced40e3
wedens-elma <wedens.elma@ui.ac.id>
2024-05-10 20:34:45 +0700

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home";
+import Login from "./pages/login";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage />,
-    },
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
