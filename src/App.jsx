5ee62649a1d4fcdd6b203742ca2eee91a913b38f
Febriansyah <61476145+blackjac7@users.noreply.github.com>
2024-05-10 21:00:59 +0700

    feature: add about page (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home";
+import About from "./pages/about";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <HomePage />,
+    },
+    {
+        path: "/about",
+        element: <About />,
     },
 ]);
 
