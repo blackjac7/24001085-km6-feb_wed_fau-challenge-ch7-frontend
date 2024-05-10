de3267b673bde0a691daba0263e0120325118e6e
Febriansyah <61476145+blackjac7@users.noreply.github.com>
2024-05-10 21:29:33 +0700

    feature: profile page (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home";
+import Profile from "./pages/profile";
 import About from "./pages/about";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <HomePage />,
+    },
+    {
+        path: "/profile",
+        element: <Profile />,
     },
     {
         path: "/about",
