a9ec39ddf1139bf0927d0bf508a1b89d0018e9d7
blackjac7 <febriansyah705@gmail.com>
2024-05-10 19:37:35 +0700

    rename: folder pages home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { HomePage } from "./pages/home";
+import HomePage from "./pages/home";
 
 const router = createBrowserRouter([
     {
