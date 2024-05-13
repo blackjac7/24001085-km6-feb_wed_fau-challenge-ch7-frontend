364482908cee5271e01f25871082570e6a850a13
Muhammad Fauzan Ramdhani <59597570+FauzanR06@users.noreply.github.com>
2024-05-13 13:03:47 +0700

    add: Pages Register (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,29 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home";
 import Profile from "./pages/profile";
 import About from "./pages/about";
+import Register from "./pages/register";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage />,
-    },
-    {
-        path: "/profile",
-        element: <Profile />,
-    },
-    {
-        path: "/about",
-        element: <About />,
-    },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
