855d48eaf148e186e76565e203b5e41eaf3f2538
Muhammad Fauzan Ramdhani <59597570+FauzanR06@users.noreply.github.com>
2024-05-15 20:41:47 +0700

    Feature register (#9)
    * add: Pages Register
    
    * add: Component Register

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,5 +1,21 @@
+import RegisterComponent from "../../components/Register";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Row>
+      <Col md={6} className="offset-md-3">
+        <Card>
+          <Card.Header>Register</Card.Header>
+          <Card.Body>
+            <RegisterComponent />
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default Register;
