9d48512f61377aac8e80712bc0c0d272a3856ed6
blackjac7 <febriansyah705@gmail.com>
2024-05-15 16:04:00 +0700

    feature: homepage v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import HomePage from "./pages/home";
 import Profile from "./pages/profile";
 import About from "./pages/about";
 import Register from "./pages/register";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/profile",
-    element: <Profile />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/profile",
+        element: <Profile />,
+    },
+    {
+        path: "/about",
+        element: <About />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
