7558daf8dc7d46c9e33581065738a871ccc656c2
Muhammad Fauzan Ramdhani <59597570+FauzanR06@users.noreply.github.com>
2024-05-17 19:03:24 +0700

    Component register v2 (#15)
    * add: Pages Register
    
    * add: Component Register
    
    * update: component-register

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,67 +1,110 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { register } from "../../redux/actions/auth";
 import Form from "react-bootstrap/Form";
 import ButtonComponent from "../../components/Button";
-import { Link } from "react-router-dom";
+import "./register.css";
+import { toast } from "react-toastify";
 
 function Register() {
-    return (
-        <Form onSubmit={""}>
-            <Form.Group className="mb-3" controlId="name">
-                <Form.Label>Name *</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter name"
-                    value={"name"}
-                    onChange={""}
-                    required
-                />
-            </Form.Group>
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-            <Form.Group className="mb-3" controlId="email">
-                <Form.Label>Email address *</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    value={"email"}
-                    onChange={""}
-                    required
-                />
-                <Form.Text className="text-muted">
-                    We will never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [gender, setGender] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [photo, setPhoto] = useState();
 
-            <Form.Group className="mb-3" controlId="password">
-                <Form.Label>Password *</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    value={"password"}
-                    onChange={""}
-                    required
-                />
-            </Form.Group>
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-            <Form.Group className="mb-3" controlId="confirmPassword">
-                <Form.Label>Confirm Password *</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Confirm Password"
-                    value={"confirmPassword"}
-                    onChange={""}
-                    required
-                />
-            </Form.Group>
+    console.log(gender);
+    if (password != confirmPassword) {
+      toast.error(`Password and confirm password must be same!`);
+      return;
+    }
 
-            <Form.Group controlId="photo" className="mb-3">
-                <Form.Label>Photo</Form.Label>
-                <Form.Control type="file" onChange={""} />
-            </Form.Group>
+    // dispatch the register action
+    dispatch(register(navigate, email, password, name, gender, photo));
+  };
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Name *</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </Form.Group>
 
-            <Link to="/reigster" className="text-center">
-                <ButtonComponent text="Register" className="button-gs" />
-            </Link>
-        </Form>
-    );
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Email address *</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <Form.Text className="text-muted">
+          We will never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="gender">
+        <Form.Label>Gender *</Form.Label>
+        <Form.Select
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          required
+        >
+          <option value="" hidden>
+            Select Gender
+          </option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </Form.Select>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="password">
+        <Form.Label>Password *</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="confirmPassword">
+        <Form.Label>Confirm Password *</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="photo" className="mb-3">
+        <Form.Label>Photo</Form.Label>
+        <Form.Control
+          type="file"
+          onChange={(e) => setPhoto(e.target.files[0])}
+        />
+      </Form.Group>
+
+      <ButtonComponent text="Register" type="submit" className="button-gs" />
+    </Form>
+  );
 }
 
 export default Register;
