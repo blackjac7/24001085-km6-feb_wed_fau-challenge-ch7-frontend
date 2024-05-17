6b05facdd762007f89c0ec8ba714f4d3fedb0a41
Wedens Elma <94461284+wedens-elma@users.noreply.github.com>
2024-05-17 21:20:44 +0700

    Feature register v2 (#19)
    * add: Pages Register
    
    * add: Component Register
    
    * fix layoutin on register form
    
    ---------
    
    Co-authored-by: FauzanR06 <fauzanm157@gmail.com>

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -20,49 +20,49 @@ function Login() {
     };
 
     return (
-        <Form onSubmit={onSubmit} className="login-form">
-            <Form.Group className="mb-3" controlId="email">
-                <Form.Label>Email address *</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <Form.Text className="text-muted">
-                    We will never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+      <Form onSubmit={onSubmit} className="login-form">
+        <h2 className="login-header">Login</h2>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email address *</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <Form.Text className="text-muted">
+            We will never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="password">
-                <Form.Label>Password *</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password *</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </Form.Group>
 
-            <ButtonComponent
-                text="Login"
-                type="submit"
-                className="button-gs btn-full-width"
-            />
+        <ButtonComponent
+          text="Login"
+          type="submit"
+          className="button-gs btn-full-width"
+        />
 
-            <GoogleLogin
-                useOneTap
-                className="google-login-button"
-                text={"Login with Google"}
-            />
-            <div className="text-center mt-3">
-                <span>Don't have an account? </span>
-                <Link to="/register">Register</Link>
-            </div>
-            
-        </Form>
+        <GoogleLogin
+          useOneTap
+          className="google-login-button"
+          text={"Login with Google"}
+        />
+        <div className="text-center mt-3">
+          <span>Don't have an account? </span>
+          <Link to="/register">Register</Link>
+        </div>
+      </Form>
     );
 }
 
--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import { Col, Row } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import ButtonComponent from "../../components/Button";
 import { register } from "../../redux/actions/auth";
@@ -21,89 +22,104 @@ function Register() {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(gender);
-    if (password != confirmPassword) {
-      toast.error(`Password and confirm password must be same!`);
+    if (password !== confirmPassword) {
+      toast.error("Password and confirm password must be same!");
       return;
     }
 
-    // dispatch the register action
-    dispatch(register(navigate, email, password, name, gender, photo));
+    dispatch(register(navigate, name, email, password, gender, photo));
   };
+
   return (
-    <Form onSubmit={onSubmit}>
-      <Form.Group className="mb-3" controlId="name">
-        <Form.Label>Name *</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </Form.Group>
+    <Form onSubmit={onSubmit} className="register-form">
+      <h2 className="register-header">Register</h2>
+      <Row>
+        <Col md="6">
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>Name *</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label>Email address *</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <Form.Text className="text-muted">
-          We will never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+          <Form.Group className="mb-0" controlId="email">
+            <Form.Label>Email address *</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <Form.Text className="text-muted small">
+              We will never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="gender">
-        <Form.Label>Gender *</Form.Label>
-        <Form.Select
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-          required
-        >
-          <option value="" hidden>
-            Select Gender
-          </option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </Form.Select>
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="gender">
+            <Form.Label>Gender *</Form.Label>
+            <Form.Select
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+              required
+            >
+              <option value="" hidden>
+                Select Gender
+              </option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </Form.Select>
+          </Form.Group>
+        </Col>
+        <Col md="6">
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password *</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="password">
-        <Form.Label>Password *</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </Form.Group>
+          <Form.Group className="mb-4" controlId="confirmPassword">
+            <Form.Label>Confirm Password *</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm your password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="confirmPassword">
-        <Form.Label>Confirm Password *</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </Form.Group>
+          <Form.Group controlId="photo" className="mb-3">
+            <Form.Label>Photo</Form.Label>
+            <Form.Control
+              type="file"
+              onChange={(e) => setPhoto(e.target.files[0])}
+            />
+          </Form.Group>
 
-      <Form.Group controlId="photo" className="mb-3">
-        <Form.Label>Photo</Form.Label>
-        <Form.Control
-          type="file"
-          onChange={(e) => setPhoto(e.target.files[0])}
-        />
-      </Form.Group>
+          </Col>
+        </Row>
+          <ButtonComponent
+            text="Register"
+            type="submit"
+            className="button-gs btn-full-width"
+          />
 
-      <ButtonComponent text="Register" type="submit" className="button-gs" />
-
+      <div className="text-center mt-3">
+        <span>Already have an account? </span>
+        <Link to="/login" className="register-link">
+          Login
+        </Link>
+      </div>
     </Form>
   );
 }
