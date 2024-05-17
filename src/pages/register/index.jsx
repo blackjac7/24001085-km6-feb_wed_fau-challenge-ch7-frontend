243d206c8cc3a124f5ce98b89c141a3bec70568e
wedens-elma <wedens.elma@ui.ac.id>
2024-05-17 21:19:06 +0700

    fix layoutin on register form

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,20 +1,14 @@
-import RegisterComponent from "../../components/Register";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import RegisterComponent from "../../components/Register";
 
 const Register = () => {
   return (
-    <Row>
-      <Col md={6} className="offset-md-3">
-        <Card>
-          <Card.Header>Register</Card.Header>
-          <Card.Body>
+      <Row>
+        <Col md={6} className="offset-md-3">
             <RegisterComponent />
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+        </Col>
+      </Row>
   );
 };
 
