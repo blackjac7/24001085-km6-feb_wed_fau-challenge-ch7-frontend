54e9376b2482217750b7a4baa2bf8b0665f67593
blackjac7 <febriansyah705@gmail.com>
2024-05-18 05:03:11 +0700

    fix-error edit profil

--- src/pages/chatroom/index.jsx
+++ src/pages/chatroom/index.jsx
@@ -9,7 +9,6 @@ import "./chatroom.css";
 const socket = io(import.meta.env.VITE_WEBSOCKET_API);
 
 const ChatRoom = () => {
-
     const dispatch = useDispatch();
 
     const [message, setMessage] = useState("");
@@ -19,163 +18,169 @@ const ChatRoom = () => {
     const userGlobal = useSelector((state) => state.auth.user);
 
     const messageTime = (date) => {
-      const messageDate = new Date(date);
-      return messageDate.toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-      });
+        const messageDate = new Date(date);
+        return messageDate.toLocaleTimeString("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
     };
 
     const sendMessage = (message) => {
-      setMessage("");
-      dispatch(
-        createMessage({
-          sender_id: userGlobal.id,
-          message_content: message,
-        })
-      );
+        setMessage("");
+        dispatch(
+            createMessage({
+                sender_id: userGlobal.id,
+                message_content: message,
+            })
+        );
     };
 
     useEffect(() => {
-      dispatch(getMessages());
+        dispatch(getMessages());
     }, [dispatch]);
 
     useEffect(() => {
-      socket.on("connect", () => {});
+        socket.on("connect", () => {});
 
-      socket.on("message", () => {
-        dispatch(getMessages());
-      });
+        socket.on("message", () => {
+            dispatch(getMessages());
+        });
 
-      socket.on("ontyping", (username) => {
-        setTyping(username + " is typing...");
-        setTimeout(() => {
-          setTyping(false);
-        }, 1000);
-      });
+        socket.on("ontyping", (username) => {
+            setTyping(username + " is typing...");
+            setTimeout(() => {
+                setTyping(false);
+            }, 1000);
+        });
     }, [dispatch]);
 
-  const messagesEndRef = useRef(null);
+    const messagesEndRef = useRef(null);
 
-  const scrollToBottom = () => {
-    if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  };
+    const scrollToBottom = () => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({
+                behavior: "smooth",
+            });
+        }
+    };
 
-  useEffect(() => {
-    dispatch(getMessages());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(getMessages());
+    }, [dispatch]);
 
-  useEffect(() => {
-    scrollToBottom(); // Scroll to bottom when messages update
-  }, [messagesGlobal]);
+    useEffect(() => {
+        scrollToBottom(); // Scroll to bottom when messages update
+    }, [messagesGlobal]);
 
-  return (
-    <Container>
-      <h1 className="mt-1 h1-chatroom">Chat Room</h1>
+    return (
+        <Container>
+            <h1 className="mt-1 h1-chatroom">Chat Room</h1>
 
-      <Container
-        style={{
-          overflowY: "auto",
-          maxHeight: "70vh",
-        }}
-      >
-        {/* get all messages */}
-        {messagesGlobal &&
-          messagesGlobal.map((message, index) => (
-            <Row
-              key={index}
-              className={`mb-3 d-flex ${
-                message.sender_id === userGlobal.id
-                  ? "flex-row-reverse"
-                  : "flex-row"
-              } align-items-start`}
-            >
-              <Col xs="auto">
-                {message.User.photo && (
-                  <Image
-                    src={message.User.photo}
-                    alt="User"
-                    roundedCircle
-                    className="user-photo"
-                  />
-                )}
-              </Col>
-              <Col xs="auto">
-                <div
-                  className={`message-content ${
-                    message.sender_id === userGlobal.id
-                      ? "align-right"
-                      : "align-left"
-                  }`}
-                >
-                  {message.User.name && (
-                    <div className="message-username">{message.User.name}</div>
-                  )}
-                  <div
-                    className={`message-bubble ${
-                      message.sender_id === userGlobal.id ? "right" : "left"
-                    }`}
-                  >
-                    <p className="message">{message.message_content}</p>
-                    <p className="message-time">
-                      {messageTime(message.createdAt)}
-                    </p>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-          ))}
-        <div ref={messagesEndRef} />
-      </Container>
-
-      <Container className="input-container">
-        <Row className="input-row">
-          {typing && (
-            <p
-              className="typing-text mb-3 ms-2"
-              style={{
-                position: "relative",
-                bottom: "35%",
-                width: "100%",
-              }}
-            >
-              {typing}
-            </p>
-          )}
-
-          {/* send message */}
-          <Col>
-            <InputGroup>
-              <input
-                className="input-chatroom"
-                type="text"
-                value={message}
-                onChange={(e) => {
-                  setMessage(e.target.value);
-                  socket.emit("typing", userGlobal.name);
+            <Container
+                style={{
+                    overflowY: "auto",
+                    maxHeight: "70vh",
                 }}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    sendMessage(message);
-                  }
-                }}
-                placeholder="Type your message here..."
-              />
-              <ButtonComponent
-                className="button-gs"
-                text="Send"
-                onClick={() => sendMessage(message)}
-              />
-            </InputGroup>
-          </Col>
-        </Row>
-      </Container>
-    </Container>
-  );
+            >
+                {/* get all messages */}
+                {messagesGlobal &&
+                    messagesGlobal.map((message, index) => (
+                        <Row
+                            key={index}
+                            className={`mb-3 d-flex ${
+                                message.sender_id === userGlobal.id
+                                    ? "flex-row-reverse"
+                                    : "flex-row"
+                            } align-items-start`}
+                        >
+                            <Col xs="auto">
+                                {message.User.photo && (
+                                    <Image
+                                        src={message.User.photo}
+                                        alt="User"
+                                        roundedCircle
+                                        className="user-photo"
+                                    />
+                                )}
+                            </Col>
+                            <Col xs="auto">
+                                <div
+                                    className={`message-content ${
+                                        message.sender_id === userGlobal.id
+                                            ? "align-right"
+                                            : "align-left"
+                                    }`}
+                                >
+                                    {message.User.name && (
+                                        <div className="message-username">
+                                            {message.User.name}
+                                        </div>
+                                    )}
+                                    <div
+                                        className={`message-bubble ${
+                                            message.sender_id === userGlobal.id
+                                                ? "right"
+                                                : "left"
+                                        }`}
+                                    >
+                                        <p className="message">
+                                            {message.message_content}
+                                        </p>
+                                        <p className="message-time">
+                                            {messageTime(message.createdAt)}
+                                        </p>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    ))}
+                <div ref={messagesEndRef} />
+            </Container>
+
+            <Container className="input-container">
+                <Row className="input-row">
+                    {typing && (
+                        <p
+                            className="typing-text mb-3 ms-2"
+                            style={{
+                                position: "relative",
+                                bottom: "35%",
+                                width: "100%",
+                            }}
+                        >
+                            {typing}
+                        </p>
+                    )}
+
+                    {/* send message */}
+                    <Col>
+                        <InputGroup>
+                            <input
+                                className="input-chatroom"
+                                type="text"
+                                value={message}
+                                onChange={(e) => {
+                                    setMessage(e.target.value);
+                                    socket.emit("typing", userGlobal.name);
+                                }}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        sendMessage(message);
+                                    }
+                                }}
+                                placeholder="Type your message here..."
+                            />
+                            <ButtonComponent
+                                className="button-gs"
+                                text="Send"
+                                onClick={() => sendMessage(message)}
+                            />
+                        </InputGroup>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
 };
 
 export default ChatRoom;
