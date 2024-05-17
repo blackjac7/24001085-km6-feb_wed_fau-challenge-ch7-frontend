4e1dfc35f85f97e1c7684b4c8330de376f048f81
Wedens Elma <94461284+wedens-elma@users.noreply.github.com>
2024-05-17 15:52:08 +0700

    Feature navbar (#11)
    * add login page
    
    * feature: add login page (remove component)
    
    * Update from comment: delete export from variable
    
    * Update index.jsx
    
    * add login component, fix login form
    
    * add navbar

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,38 +1,43 @@
+import React from "react";
 import Form from "react-bootstrap/Form";
 import ButtonComponent from "../../components/Button";
+import "./login.css"; // Make sure this path is correct
 
 function Login() {
 	return (
-		<Form onSubmit={""}>
-			<Form.Group className="mb-3" controlId="email">
-				<Form.Label>Email address *</Form.Label>
-				<Form.Control
-					type="email"
-					placeholder="Enter your email"
-					onChange={""}
-					required
-				/>
-				<Form.Text className="text-muted">
-					We will never share your email with anyone else.
-				</Form.Text>
-			</Form.Group>
+		<div className="login-container">
+			<h2 className="login-header">Login</h2>
+			<Form onSubmit={""}>
+				<Form.Group className="mb-3" controlId="email">
+					<Form.Label>Email address *</Form.Label>
+					<Form.Control
+						type="email"
+						placeholder="Enter your email"
+						onChange={""}
+						required
+					/>
+					<Form.Text className="text-muted form-text">
+						We will never share your email with anyone else.
+					</Form.Text>
+				</Form.Group>
 
-			<Form.Group className="mb-3" controlId="password">
-				<Form.Label>Password *</Form.Label>
-				<Form.Control
-					type="password"
-					placeholder="Enter your password"
-					onChange={""}
-					required
-				/>
-			</Form.Group>
+				<Form.Group className="mb-3" controlId="password">
+					<Form.Label>Password *</Form.Label>
+					<Form.Control
+						type="password"
+						placeholder="Enter your password"
+						onChange={""}
+						required
+					/>
+				</Form.Group>
 
-			<ButtonComponent
-				text="Login"
-				type="button-gs"
-				className="btn-full-width"
-			/>
-		</Form>
+				<ButtonComponent
+					text="Login"
+					type="button-gs"
+					className="btn-full-width"
+				/>
+			</Form>
+		</div>
 	);
 }
 
